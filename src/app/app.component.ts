a8d3b83aad6f1428392f4a389bd06e6b2edf4e08
Héctor Vizcaíno <hektor7@gmail.com>
2017-06-27 19:32:58 +0200

    We've separate hero-detail and hero class to be reusable and manteinable. Next step: https://angular.io/tutorial/toh-pt4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,5 @@
-import { Component } from '@angular/core';
-
-export class Hero{
-  id: number;
-  name: string;
-}
+import { Component, Input } from '@angular/core';
+import { Hero } from './hero';
 
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
@@ -49,7 +45,7 @@ const HEROES: Hero[] = [
     background-color: #DDD;
     left: .1em;
   }
-  .heroes .text {
+  .heroes .text { 
     position: relative;
     top: -3px;
   }
@@ -79,15 +75,8 @@ const HEROES: Hero[] = [
             <span class="badge">{{hero.id}}</span> {{hero.name}}
           </li>
         </ul>
-      <div *ngIf="selectedHero">
-        <h2>{{selectedHero.name}} details!</h2>
-        <div><label>id: </label>{{selectedHero.id}}</div>
-        <div>
-             <label>name: </label>
-             <input [(ngModel)]="selectedHero.name" placeholder="name">
-        </div>
-      </div>
-  `,
+      <hero-detail [hero]="selectedHero"></hero-detail>
+        `,
 })
 export class AppComponent  {
   heroes = HEROES;
